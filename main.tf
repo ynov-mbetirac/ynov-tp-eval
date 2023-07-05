884664162b5dfc33873bed6492bd1fb1552be2fc
Maxime BETIRAC <maxime.betirac@ynov.com>
2023-07-05 15:56:15 +0000

    commit pour backup

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ variable "ssh_host" {}
 variable "ssh_key" {}
 variable "password" {}
 
-#module "deploy_openstack" {
-#    source = "./modules/deploy_openstack"
-#    ssh_host = var.ssh_host
-#    ssh_user = var.ssh_user
-#    ssh_key = var.ssh_key
-#    password = var.password
-#}
+module "deploy_openstack" {
+    source = "./modules/deploy_openstack"
+    ssh_host = var.ssh_host
+    ssh_user = var.ssh_user
+    ssh_key = var.ssh_key
+    password = var.password
+}
 
 module "deploy_ubuntu" {
     source = "./modules/deploy_ubuntu"
--- modules/deploy_ubuntu/main.tf
+++ modules/deploy_ubuntu/main.tf
@@ -12,6 +12,20 @@ provider "openstack" {
   cloud = "openstack"
 }
 
+resource "openstack_networking_subnet_v2" "tp-ynov-subnet" {
+  name       = "tp-ynov-subnet"
+  network_id = openstack_networking_network_v2.network_1.id
+  cidr       = "10.0.0.0/24"
+  ip_version = 4
+}
+
+resource "openstack_blockstorage_volume_v2" "volume_1" {
+  region      = "RegionOne"
+  name        = "volume_1"
+  description = "first test volume"
+  size        = 10
+}
+
 resource "openstack_compute_instance_v2" "ubuntu22" {
   name = element(var.instance_name, count.index)
   count = var.instance_count
@@ -19,8 +33,9 @@ resource "openstack_compute_instance_v2" "ubuntu22" {
   flavor_id = "c9a3d00a-f2b5-49a0-8326-0fbe0e18e00f"
   key_pair = "formation-keypair"
   security_groups = ["default"]
+  volume_id = openstack_blockstorage_volume_v2.volume_1.id
 
   network {
-    name = "tp-ynov-net"
+    name = openstack_networking_network_v2.network_1.id
   }
 }
